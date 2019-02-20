30e8b0e8dda18c5275affbf5a8ad208eb7e34a8c
wayoalamos <malamos2@uc.cl>
2019-02-20 16:34:53 -0300

    try

--- app.py
+++ app.py
@@ -49,8 +49,29 @@ def waiting(task_id):
     result = job.result
     # write the workbook here with information in the result
 
+    my_workbook = Workbook()
 
-    output = make_response(openpyxl.writer.excel.save_virtual_workbook(result))
+    #s.workbook_active = s.workbook.active
+    my_workbook_active = my_workbook.active
+
+    # excel headers
+    header=["Tipo", "Categoria", "Ubicacion", "Codigo", "Informacion",
+    "Construido", "Terreno", "Valor(UF)", "UF/Construido", "UF/Terreno", "url"]
+    #s.workbook_active.append(header)
+    my_workbook_active.append(header)
+
+    # aca esta el problema
+
+
+    print("se termino la busqueda por url")
+    for j in result:
+        print (j)
+        my_workbook_active.append(j)
+
+
+
+
+    output = make_response(openpyxl.writer.excel.save_virtual_workbook(my_workbook))
     output.headers["Content-Disposition"] = "attachment; filename=Datos.xlsx"
     output.headers["Content-type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     print("excel esta listo para ser retornado suerte")
--- utils.py
+++ utils.py
@@ -1,36 +1,7 @@
-from search import Search
-from openpyxl import Workbook
-from flask import make_response
-import openpyxl
-import pyexcel
-
-from search import Search
-from openpyxl import Workbook
-
-import time
-
-
 def count_words_at_url(url):
     s = Search() # create search element
     #s.workbook = Workbook() # create workbook element
-    my_workbook = Workbook()
-
-    #s.workbook_active = s.workbook.active
-    my_workbook_active = my_workbook.active
-
-    # excel headers
-    header=["Tipo", "Categoria", "Ubicacion", "Codigo", "Informacion",
-    "Construido", "Terreno", "Valor(UF)", "UF/Construido", "UF/Terreno", "url"]
-    #s.workbook_active.append(header)
-    my_workbook_active.append(header)
-
-    # aca esta el problema
     s.find_products(url) # find products of the urls
 
-    print("se termino la busqueda por url")
-    for j in s.data:
-        print (j)
-        my_workbook_active.append(j)
-
     # workbook = s.workbook
-    return my_workbook
+    return s.data
