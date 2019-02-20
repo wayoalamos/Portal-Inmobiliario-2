5f12102af92b256f4724aebf490f9ddc48ed2ba9
wayoalamos <malamos2@uc.cl>
2019-02-20 16:51:19 -0300

    try

--- utils.py
+++ utils.py
@@ -3,4 +3,4 @@ from search import Search
 def count_words_at_url(url):
     s = Search() # create search element
     s.find_products(url) # find products of the urls
-    return s.data
+    return "s.data"
